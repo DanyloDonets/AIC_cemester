47e1b6911e54d4f88b94944162e773f0ef305c59
Danylo Donets <EndoBroYT@gmail.com>
2022-06-14 00:44:47 +0300

    final task

---
+++ Board_Shop/Board_Shop/Boat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Boat_Shop
+{
+    public class Boat
+    {
+        [Key]
+        public int Boat_number { get; set; }
+        public string Boat_Name { get; set; }
+        public int Price { get; set; }
+        public int Id { get; set; }
+        public Type Type { get; set; }
+
+
+
+
+    }
+}
